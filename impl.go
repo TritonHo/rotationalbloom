cd64c6c36e83413e464ae5c0bb625e26077cf8b0
Triton Ho <>
2020-04-04 19:38:54 +0800

    fix various bug

--- impl.go
+++ impl.go
@@ -1,7 +1,8 @@
 package rotationalbloom
 
 import (
-	"log"
+	//	"log"
+
 	"strconv"
 	"sync"
 	"time"
@@ -50,15 +51,9 @@ func (im *impl) GetAppxCount() int {
 	im.metaLock.RLock()
 	b := im.currentBloom.Clone()
 	b.Merge(im.aggregatedOldBloom)
-
-	locations := []int{}
-	for loc := range im.recentLocations {
-		locations = append(locations, loc)
-	}
 	im.metaLock.RUnlock()
 
 	// use the local variables to compute the result
-	b.AddLocations(locations)
 	return b.GetAppxCount()
 }
 
@@ -66,7 +61,7 @@ func (im *impl) Check(s string) bool {
 	im.metaLock.RLock()
 	defer im.metaLock.RUnlock()
 
-	return im.currentBloom.Check(s)
+	return im.currentBloom.Check(s) || im.aggregatedOldBloom.Check(s)
 }
 
 // add s to the bloom
@@ -89,7 +84,6 @@ func (im *impl) addWorkerThread() {
 		im.metaLock.RUnlock()
 	}
 }
-
 func (im *impl) syncWithRedis() {
 	im.metaLock.RLock()
 	defer im.metaLock.RUnlock()
@@ -97,12 +91,27 @@ func (im *impl) syncWithRedis() {
 	currentTime := time.Now()
 
 	// step 1: merge the redis bloom to local current bloom
-	im.syncFromRedis(currentTime, im.currentBloom)
+	im.downloadFromRedis(currentTime, im.currentBloom)
 
 	// step 2: sync the location to redis
-	im.syncLocations(currentTime, im.currentBloom, im.recentLocations, false)
-}
+	im.locationLock.RLock()
+	candidates, toBeDeleted := im.getUploadCandidates(im.currentBloom, im.recentLocations, false)
+	im.locationLock.RUnlock()
+	if err := im.uploadLocations(currentTime, candidates); err != nil {
+		// FIXME: handle the error
+	} else {
+		toBeDeleted = append(toBeDeleted, candidates...)
+	}
 
+	// step 3: remove the uploaded / no longer needed locations
+	if len(toBeDeleted) > 0 {
+		im.locationLock.Lock()
+		for _, loc := range toBeDeleted {
+			delete(im.recentLocations, loc)
+		}
+		im.locationLock.Unlock()
+	}
+}
 func (im *impl) redisKeyName(t time.Time) string {
 	ts := t.Unix()
 	interval := int64(im.config.Interval.Seconds())
@@ -112,34 +121,42 @@ func (im *impl) redisKeyName(t time.Time) string {
 	return im.config.KeyPrefix + strconv.FormatInt(ts, 10)
 }
 
-// sync the data from the redis to target bloom
-func (im *impl) syncFromRedis(t time.Time, b *bloomfilter) {
+// download the data from the redis to target bloom
+func (im *impl) downloadFromRedis(t time.Time, b *bloomfilter) {
 	// FIXME: implement it
 }
 
-// sync the location to the redis
-func (im *impl) syncLocations(t time.Time, b *bloomfilter, locations map[int]bool, isAll bool) {
-	redisKey := im.redisKeyName(t)
+// upload the location to the redis
 
-	im.locationLock.Lock()
-	defer im.locationLock.Unlock()
+// get the upload candidates from the input
+func (im *impl) getUploadCandidates(b *bloomfilter, locations map[int]bool, isAll bool) (candidates []int, toBeDeleted []int) {
+	candidates = []int{}
+	toBeDeleted = []int{}
+
 	// FIXME: implement the non-all version
-
-	redisAgrs := []interface{}{}
-
+	candidateLocations := []int{}
 	for loc, _ := range locations {
 		if b.TestLocation(loc) {
-			delete(locations, loc)
+			toBeDeleted = append(toBeDeleted, loc)
 		} else {
-			redisAgrs = append(redisAgrs, `set`, `u1`, loc, 1)
+			candidateLocations = append(candidateLocations, loc)
 		}
 	}
-	if len(redisAgrs) > 0 {
-		if err := im.redisClient.BitField(redisKey, redisAgrs...).Err(); err != nil {
-			// FIXME: handle the error
-			log.Println(err)
-		}
+	return
+}
+func (im *impl) uploadLocations(t time.Time, candidates []int) error {
+	// sentinal
+	if len(candidates) == 0 {
+		return nil
 	}
+
+	redisAgrs := []interface{}{}
+	for _, loc := range candidates {
+		redisAgrs = append(redisAgrs, `set`, `u1`, loc, 1)
+	}
+
+	redisKey := im.redisKeyName(t)
+	return im.redisClient.BitField(redisKey, redisAgrs...).Err()
 }
 func (im *impl) rebuildAgg() {
 	im.aggregatedOldBloom = NewBloom(im.m, im.k)
@@ -158,7 +175,7 @@ func (im *impl) rotation(currentTime time.Time) {
 
 	// step 1: swap the new locations
 	im.locationLock.Lock()
-	oldLocations := im.recentLocations
+	prevLocations := im.recentLocations
 	im.recentLocations = map[int]bool{}
 	im.locationLock.Unlock()
 
@@ -169,13 +186,17 @@ func (im *impl) rotation(currentTime time.Time) {
 	im.metaLock.Unlock()
 
 	// step 3: sync back all the location back to redis
-	im.syncLocations(oldTime, im.oldBlooms[im.oldestIndex], oldLocations, true)
+	im.metaLock.RLock()
+	candidates, _ := im.getUploadCandidates(im.oldBlooms[im.oldestIndex], prevLocations, true)
+	im.metaLock.RUnlock()
+	im.uploadLocations(oldTime, candidates)
 
 	// step 4: sleep for 5 seconds to ensure all mechanism finished the upload of pervious interval's location
 	time.Sleep(previousIntervalWaitTime)
 
+	// FIXME: there is race condition here
 	// step 5: download the finalized pervious interval's bloom from redis
-	im.syncFromRedis(oldTime, im.oldBlooms[im.oldestIndex])
+	im.downloadFromRedis(oldTime, im.oldBlooms[im.oldestIndex])
 
 	// step 6: rebuild the agg
 	im.metaLock.Lock()
